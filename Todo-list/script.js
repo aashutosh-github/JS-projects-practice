4525e08d2a16debf7a4534692ae893100352e75f
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-09-27 17:24:28 +0530

    Feat: add event listener to the add task button

--- Todo-list/script.js
+++ Todo-list/script.js
@@ -2,4 +2,17 @@ const input = document.getElementById("todo-input");
 const addTaskButton = document.getElementById("add-task-btn");
 const todoList = document.getElementById("todo-list");
 
-let todo = [];
+let tasks = [];
+
+addTaskButton.addEventListener("click", () => {
+  const task = input.value.trim();
+  if (task === "") return;
+  const data = {
+    //the date now method will return a new (and unique) string always as time does not stop.
+    id: Date.now(),
+    text: task,
+    completed: false,
+  };
+  tasks.push(data);
+  input.value = ""; //clear the input box
+});
