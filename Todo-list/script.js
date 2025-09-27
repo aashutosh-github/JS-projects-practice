93415807ea70da16855350eb97d2d3daab9ff237
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-09-27 14:52:00 +0530

    Initialise: script.js file

---
+++ Todo-list/script.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+let todo = [];
