e0520e899e14d9d7cc41d5d2d696c42fa92999b0
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-09-27 19:55:51 +0530

    Fix: removed the DOM loaded listener and replaced the window reload with displayTasks function. Also added the delete button functionality

--- Todo-list/script.js
+++ Todo-list/script.js
@@ -1,56 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const input = document.getElementById("todo-input");
-  const addTaskButton = document.getElementById("add-task-btn");
-  const todoList = document.getElementById("todo-list");
+const input = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
 
-  //doing all this once the DOM gets loaded so that we can successfully load the items from the
-  //local storage to this array
-  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  tasks.forEach((task) => displayTasks(task));
+tasks.forEach((task) => displayTasks(task));
 
-  addTaskButton.addEventListener("click", () => {
-    const task = input.value.trim();
-    if (task === "") return;
-    const data = {
-      //the date now method will return a new (and unique) string always as time does not stop.
-      id: Date.now(),
-      text: task,
-      completed: false,
-    };
-    tasks.push(data);
-    //since all the previous data of local storage is overridden completely when we update it,
-    //we can safely save the tasks to local storage without worrying about the data being copied multiple times
-    //as the previous data gets wiped out and then the new data gets updated.
-    saveToLocalStorage();
-    input.value = ""; //clear the input box
-    //new tasks are added to local storage but only get displayed once the DOM gets reloaded so we are
-    //reloading the DOM after clicking on this button so that the list items show up.
-    window.location.reload();
-  });
+addTaskButton.addEventListener("click", () => {
+  const task = input.value.trim();
+  if (task === "") return;
+  const data = {
+    //the date now method will return a new (and unique) string always as time does not stop.
+    id: Date.now(),
+    text: task,
+    completed: false,
+  };
+  tasks.push(data);
+  //since all the previous data of local storage is overridden completely when we update it,
+  //we can safely save the tasks to local storage without worrying about the data being copied multiple times
+  //as the previous data gets wiped out and then the new data gets updated.
+  saveToLocalStorage();
+  input.value = ""; //clear the input box
+  displayTasks(data);
+});
 
-  function saveToLocalStorage() {
-    //since the local storage only accepts the key value pairs (both of them) in the form of strings,
-    //we have to convert the array to JSON format
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }
+function saveToLocalStorage() {
+  //since the local storage only accepts the key value pairs (both of them) in the form of strings,
+  //we have to convert the array to JSON format
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
-  function displayTasks(task) {
-    const li = document.createElement("li");
-    li.setAttribute("data-id", task.id);
-    li.innerHTML = `
+function displayTasks(task) {
+  const li = document.createElement("li");
+  li.setAttribute("data-id", task.id);
+  li.innerHTML = `
   <span>${task.text}</span>
   <button>Delete</button>
   `;
-    li.addEventListener("click", (event) => {
-      if (event.target.tagName === "BUTTON") return;
-      task.completed = !task.completed;
-      li.classList.toggle("completed");
-      //since some properties have been modified so now we need to again save the
-      //updated data in the local storage.
-      saveToLocalStorage();
-    });
-    if (task.completed) li.classList.add("completed");
-    todoList.appendChild(li);
-  }
-});
+  li.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") return;
+    task.completed = !task.completed;
+    li.classList.toggle("completed");
+    //since some properties have been modified so now we need to again save the
+    //updated data in the local storage.
+    saveToLocalStorage();
+  });
+  if (task.completed) li.classList.add("completed");
+
+  li.querySelector("button").addEventListener("click", (event) => {
+    //this is to stop event bubbling.
+    event.stopPropagation();
+    tasks = tasks.filter((t) => t.id !== task.id);
+    saveToLocalStorage();
+    li.remove();
+  });
+  todoList.appendChild(li);
+}
