ff7ea19d4c1a63628735f58e5bba90e8ef423922
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-09-29 18:50:47 +0530

    Chore: Add all the DOM elements in JS file

---
+++ API-handling/script.js
@@ -0,0 +1,7 @@
+const cityInput = document.querySelector("#city-input");
+const getWeatherButton = document.querySelector("#get-weather-btn");
+const weatherInfo = document.querySelector("#weather-info");
+const cityNameDisplay = document.querySelector("#city-name");
+const description = document.querySelector("#description");
+const temperature = document.querySelector("#temperature");
+const errorMessage = document.querySelector("#error-message");
