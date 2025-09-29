6814460c1dc5af1d0db8120f6b91f607e3ab45b8
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-09-29 19:11:48 +0530

    Feat: Add event listener for get weather button

--- API-handling/script.js
+++ API-handling/script.js
@@ -5,3 +5,30 @@ const cityNameDisplay = document.querySelector("#city-name");
 const description = document.querySelector("#description");
 const temperature = document.querySelector("#temperature");
 const errorMessage = document.querySelector("#error-message");
+
+getWeatherButton.addEventListener("click", async () => {
+  let city = cityInput.value.trim();
+  if (!city) return;
+
+  //it may throw an error so use try catch
+  //server may be in another continent so it will take some time
+  try {
+    const weatherData = await fetchWeatherData(city);
+    displayWeatherData(weatherData);
+  } catch (error) {
+    showError(error);
+  }
+});
+
+async function fetchWeatherData(city) {
+  //get the data
+}
+
+function displayWeatherData(weatherData) {
+  //display the data
+}
+
+function showError() {
+  weatherInfo.classList.add("hidden");
+  errorMessage.classList.remove("hidden");
+}
