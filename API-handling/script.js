62ecab632d1813646a863b1deacd2e5b059f109f
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-09-29 20:29:46 +0530

    Complete: all functionality of API handling project completed

--- API-handling/script.js
+++ API-handling/script.js
@@ -13,22 +13,50 @@ getWeatherButton.addEventListener("click", async () => {
   //it may throw an error so use try catch
   //server may be in another continent so it will take some time
   try {
-    const weatherData = await fetchWeatherData(city);
-    displayWeatherData(weatherData);
+    const data = await getParamsUsingCity(city);
+    const cityData = data.results[0];
+    const { latitude, longitude } = cityData;
+    const temperatureData = await fetchTemperatureData(latitude, longitude);
+    displayWeatherData(city, temperatureData.toFixed(2));
   } catch (error) {
-    showError(error);
+    showError();
   }
 });
 
-async function fetchWeatherData(city) {
+async function fetchTemperatureData(latitude, longitude) {
   //get the data
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_mean`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const weatherData = data.daily.temperature_2m_mean;
+    return (
+      weatherData.reduce((accumulator, current) => {
+        return accumulator + current;
+      }, 0) / weatherData.length
+    );
+  } catch (error) {
+    showError();
+  }
 }
 
-function displayWeatherData(weatherData) {
+function displayWeatherData(city, temp) {
   //display the data
+  weatherInfo.classList.remove("hidden");
+  cityNameDisplay.innerText = `City: ${city}`;
+  temperature.innerHTML = `Temperature: ${temp}&deg;C`;
 }
 
 function showError() {
   weatherInfo.classList.add("hidden");
   errorMessage.classList.remove("hidden");
+}
+
+async function getParamsUsingCity(city) {
+  //get latitude and longitude from the city name
+  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`;
+  const response = await fetch(url);
+  if (!response.ok) showError();
+  const dataOfCity = await response.json();
+  return dataOfCity;
 }
