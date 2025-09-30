1d9410521520b2407c97861ab5166a5064c762ba
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-09-30 20:58:19 +0530

    Chore: select DOM elements to initialise JS file

---
+++ Add-to-cart-page/script.js
@@ -0,0 +1,6 @@
+const productList = document.querySelector("#product-list");
+const cartItems = document.querySelector("#cart-items");
+const emptyCart = document.querySelector("#empty-cart");
+const cartTotal = document.querySelector("#cart-total");
+const totalPrice = document.querySelector("#total-price");
+const checkoutButton = document.querySelector("#checkout-btn");
