5925040a2f4797db354cc16dd87344641bc12859
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-10-03 22:49:21 +0530

    Chore: Select all the important DOM elements

---
+++ Quiz-application/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const questionContainer = document.querySelector("#question-container");
+  const questionText = document.querySelector("#question-text");
+  const choicesList = document.querySelector("#choices-list");
+  const nextButton = document.querySelector("#next-btn");
+  const resultContainer = document.querySelector("#result-container");
+  const scoreDisplay = document.querySelector("#score");
+  const restartButton = document.querySelector("#restart-btn");
+  const startButton = document.querySelector("#start-btn");
+});
